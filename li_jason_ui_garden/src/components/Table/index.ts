2407a6bdfadc003266e2dd98a6512740ce9c0cff
Jason Li <senli@rrc.ca>
2025-07-03 17:24:44 -0500

    [update]storybook runs properly with test

---
+++ li_jason_ui_garden/src/components/Table/index.ts
@@ -0,0 +1,3 @@
+export { Table, TableHeader, TableRow, TableCell, TableFooter } from './Table';
+export type { TableProps, TableHeaderProps, TableRowProps, TableCellProps, TableFooterProps } from './Table.types';
+export * from './Table.stories';
